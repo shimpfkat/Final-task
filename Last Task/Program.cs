7987349dea1bfa84668f1423460906fa7669aa70
shimpfkat <155801855+shimpfkat@users.noreply.github.com>
2024-03-02 13:19:38 +0500

    Add files via upload

---
+++ Last Task/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] arr1 = new string[4] {"Hello", "2", ":-)", "world"};
+string[] arr2 = new string[arr1.Length];
+void SecondArrayWithIF(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+    if(arr1[i].Length <= 2)
+        {
+        arr2[count] = arr1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(arr1, arr2);
+PrintArray(arr2);
